6e7f12f7cc52ae52da74b71a5c14d17c18e2a430
tinachri <tina.syversen@gmail.com>
2013-04-16 13:41:51 +0200

    Lagd get og set til spm pluss metode for å vise spm

--- src/sheeprace/derp/Question.java
+++ src/sheeprace/derp/Question.java
@@ -1,5 +1,8 @@
 package sheeprace.derp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This is the question. It should be created by a class implementing the
  * QuestionMakerInterface 
@@ -9,15 +12,25 @@ package sheeprace.derp;
  */
 
 public class Question implements QuestionInterface{
-
+	
+	private String question;
+	private List<String> options = new ArrayList<String>();
+	private List<Boolean> correctA = new ArrayList<Boolean>();
+	
 	@Override
 	public void setQuestion(String question) {
+		this.question = "Er Trondheim verdens beste studentby?";
 		
 	}
 
+	public String getQuestion(){
+		return question;
+	}
+	
 	@Override
-	public void addOption(String opiton, boolean correct) {
-		
+	public void addOption(String option, boolean correct) {
+		options.add(option);
+		correctA.add(correct);
 	}
 
 }
