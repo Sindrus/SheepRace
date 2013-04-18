2c7a5f3551fdb8fba120efed809ba682e6d0d649
Sindrus <sindreny@gmail.com>
2013-04-18 13:57:32 +0200

    added docs to questoinmaker

--- src/sheeprace/derp/QuestionMaker.java
+++ src/sheeprace/derp/QuestionMaker.java
@@ -17,53 +17,71 @@ import android.util.Log;
 public class QuestionMaker{
 	private static final String TAG = "CompXML";
 	
+	/**
+	 * Returns a questionobject back to the method asking for it. 
+	 * @param main need the MainActivity to access the xml-sheet
+	 * @param qid Need to get an ID for what question to get
+	 * @param category From what category shoudl the question come from?
+	 * @return a Questionobject, loaded with a question and options.
+	 */
 	public static Question createQuestion(MainActivity main, int qid, String category) {
 		try {
 			
 			XmlResourceParser xrp = main.getResources().getXml(R.xml.question);
 			
+			// Running through the xml-sheet until it reaches the end
 			while(xrp.getEventType() != XmlResourceParser.END_DOCUMENT){
 				if(xrp.getEventType() == XmlResourceParser.START_TAG){
+					
+					// Check if the next questions are in the correct category
 					if(xrp.getName().equals("q") && xrp.getAttributeValue(0).equals(category)){
 						System.out.println("I riktig kategori");
+						
+						// Get the number of questions in the category
 						int qs = xrp.getAttributeIntValue(1, -1);
 						System.out.println("qs: "+ qs);
 						while(xrp.getEventType() != XmlResourceParser.END_DOCUMENT){
 							
+							// FIXME: do this in a nicer way
 								String s="";
 								s = xrp.getName();
+								// Apparently it is impossible to compare a 
+								// string with value null to another string
 								if(s==null)
 									s="";
 								
+								// Have we found a questiontag?
 								if(s.equals("question"))
-								if(xrp.getAttributeIntValue(0, -1)==qid){
-									
-									System.out.println("Parsing question");
-									Question q = new Question();
-									xrp.next();
-									
-									String title = xrp.nextText();
-									System.out.println("the title is: "+title);
-									
-									q.setQuestion(title);
-									xrp.next();
-									
-									while (xrp.getName().equals("option")){
-										xrp.next();
-										String answer = xrp.nextText();
+									// Indeed we have, is it the correct question?
+									if(xrp.getAttributeIntValue(0, -1)==qid){
+										
+										System.out.println("Parsing question");
+										// Create the new question
+										Question q = new Question();
 										xrp.next();
 										
-										String corr = xrp.nextText();
-										boolean tf = corr.equals("true");
-										
+										String title = xrp.nextText();
+										System.out.println("the title is: "+title);
+										// Set the question
+										q.setQuestion(title);
 										xrp.next();
-										xrp.next();
-										q.addOption(answer, tf);
+										// Look for answeroptions to the question 
+										while (xrp.getName().equals("option")){
+											xrp.next();
+											String answer = xrp.nextText();
+											xrp.next();
+											
+											String corr = xrp.nextText();
+											boolean tf = corr.equals("true");
+											
+											xrp.next();
+											xrp.next();
+											q.addOption(answer, tf);
+										}
+										System.out.println(q.toString());
+										// And we are done!
+										return q;
 									}
-									System.out.println(q.toString());
-									return q;
-								}
-							//}
 							xrp.next();
 						}
 					}else
@@ -80,6 +98,7 @@ public class QuestionMaker{
 			Log.e(TAG, "Unable to read resource file");
 			ioe.printStackTrace();
 		}
+		// The question we were looking for does not exist. What to do?!
 		return null;
 	}
 }
