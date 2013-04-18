93bc3f58493852367035d9caa1b143ddfe425ac0
Sindrus <sindreny@gmail.com>
2013-04-18 13:39:39 +0200

    questionparsing with category working

--- src/sheeprace/derp/QuestionMaker.java
+++ src/sheeprace/derp/QuestionMaker.java
@@ -17,36 +17,57 @@ import android.util.Log;
 public class QuestionMaker{
 	private static final String TAG = "CompXML";
 	
-	public static Question createQuestion(MainActivity main, int qid) {
+	public static Question createQuestion(MainActivity main, int qid, String category) {
 		try {
 			
 			XmlResourceParser xrp = main.getResources().getXml(R.xml.question);
 			
 			while(xrp.getEventType() != XmlResourceParser.END_DOCUMENT){
 				if(xrp.getEventType() == XmlResourceParser.START_TAG){
-					
-					String s = xrp.getName();
-					if(s.equals("question") && xrp.getAttributeIntValue(0, -1)==qid){
-						System.out.println("Parsing question");
-						Question q = new Question();
-						xrp.next();
-						String title = xrp.nextText();
-						System.out.println("the title is: "+title);
-						q.setQuestion(title);
-						xrp.next();
-						System.out.println("Now at: "+xrp.getName());
-						while (xrp.getName().equals("option")){
+					if(xrp.getName().equals("q") && xrp.getAttributeValue(0).equals(category)){
+						System.out.println("I riktig kategori");
+						int qs = xrp.getAttributeIntValue(1, -1);
+						System.out.println("qs: "+ qs);
+						while(xrp.getEventType() != XmlResourceParser.END_DOCUMENT){
+							
+								String s="";
+								s = xrp.getName();
+								if(s==null)
+									s="";
+								
+								if(s.equals("question"))
+								if(xrp.getAttributeIntValue(0, -1)==qid){
+									
+									System.out.println("Parsing question");
+									Question q = new Question();
+									xrp.next();
+									
+									String title = xrp.nextText();
+									System.out.println("the title is: "+title);
+									
+									q.setQuestion(title);
+									xrp.next();
+									
+									while (xrp.getName().equals("option")){
+										xrp.next();
+										String answer = xrp.nextText();
+										xrp.next();
+										
+										String corr = xrp.nextText();
+										boolean tf = corr.equals("true");
+										
+										xrp.next();
+										xrp.next();
+										q.addOption(answer, tf);
+									}
+									System.out.println(q.toString());
+									return q;
+								}
+							//}
 							xrp.next();
-							String answer = xrp.nextText();
-							xrp.next();
-							String corr = xrp.nextText();
-							boolean tf = corr.equals("true");
-							xrp.next();
-							xrp.next();
-							q.addOption(answer, tf);
 						}
-					}
-					xrp.next();
+					}else
+						xrp.next();
 				}
 				xrp.next();
 			}
