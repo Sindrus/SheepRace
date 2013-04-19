42a353ec9e1ef2fb0d01ee1e57b1ced0096e23b6
Sindrus <sindreny@gmail.com>
2013-04-19 14:07:35 +0200

    changed a few things in the code and added docs

--- src/sheeprace/derp/LevelMaker.java
+++ src/sheeprace/derp/LevelMaker.java
@@ -13,18 +13,26 @@ public class LevelMaker{
 	public static Level createLevel(MainActivity main, String theme, int levelnum) {
 		Level l = new Level();
 		try{
-			
+			// Initiating xmlresourceparser
 			XmlResourceParser xrp = main.getResources().getXml(R.xml.level);
+			
+			// Go through the document
 			while(xrp.getEventType() != xrp.END_DOCUMENT){
 				if(xrp.getEventType() == XmlResourceParser.START_TAG){
+					// check for starttag for levels with themes
 					if(xrp.getName().equals("levels"))
 					if(xrp.getAttributeValue(0).equals(theme)){
+						// Must not run out of the document
 						while(xrp.getEventType() != xrp.END_DOCUMENT){
+							// Check to see if we found the level we are looking for
 							if(xrp.getName().equals("level")){
-								System.out.println("Level funnet");
 								if(xrp.getAttributeIntValue(0, -1)==levelnum){
 									xrp.next();
-									while(xrp.getEventType() != xrp.END_DOCUMENT){
+									
+									// Get all the elements on that level
+									while(!xrp.getName().equals("end") || 
+											xrp.getEventType() != xrp.END_DOCUMENT){
+										System.out.println("getName(): "+xrp.getName());
 										if(xrp.getName().equals("qbox")){
 											float x = Float.parseFloat(xrp.getAttributeValue(0));
 											float y = Float.parseFloat(xrp.getAttributeValue(1));
@@ -33,13 +41,12 @@ public class LevelMaker{
 											float x = Float.parseFloat(xrp.getAttributeValue(0));
 											float y = Float.parseFloat(xrp.getAttributeValue(1));
 											l.addBBox(new BlockBox(x, y));
-										}else if(xrp.getName().equals("end")){
-											System.out.println(l.toString());
-											return l;
 										}
 										xrp.next();
 										xrp.next();
 									}
+									System.out.println(l.toString());
+									return l;
 								}
 							}
 							xrp.next();
@@ -54,6 +61,7 @@ public class LevelMaker{
 			e.printStackTrace();
 			System.err.println("An error has occured! Oh noooo!");
 		}
+		System.out.println("Couldn't create the level, donno what happened.");
 		return null;
 	}
 
