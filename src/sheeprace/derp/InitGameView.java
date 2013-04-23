2d2a9056836a1e1c8c4cfe0dc66142a280e2b5d5
Hanne <hannekottum@gmail.com>
2013-04-23 21:25:38 +0200

    only one player in initview at the time, both players can be initialized

--- src/sheeprace/derp/InitGameView.java
+++ src/sheeprace/derp/InitGameView.java
@@ -10,7 +10,6 @@ import android.graphics.Paint.Align;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 
 import sheep.game.State;
 import sheep.graphics.Font;
@@ -28,12 +27,11 @@ public class InitGameView extends State implements KeyboardListener{
 
 	private int index1, index2;
 	private MainActivity main;
-	private TextButton backButton, startGame, savePlayer1, savePlayer2;
+	private TextButton backButton, startGame, savePlayer, player1ReadyButton;
 	private Font font;
-	private String player1Name = "";
-	private String player2Name = "";
+	private String playerName = "";
 	private Level l;
-	private Player sheepPlayer1, sheepPlayer2;
+	private Player sheepPlayer1;
 	private boolean player1Ready, player2Ready;
 	private InputMethodManager imm;
 	
@@ -45,20 +43,19 @@ public class InitGameView extends State implements KeyboardListener{
 		
 		backButton = new TextButton(50, 50, "Back");
 		startGame = new TextButton(3*(Constants.WINDOW_WIDTH/4), 50, "Start Game");
-		savePlayer1 = new TextButton(Constants.WINDOW_WIDTH/4, Constants.WINDOW_HEIGHT/2, "Save Player");
+		savePlayer = new TextButton(Constants.WINDOW_WIDTH/4, Constants.WINDOW_HEIGHT/2, "Save Player");
+		player1ReadyButton = new TextButton(Constants.WINDOW_WIDTH/2, 2*Constants.WINDOW_HEIGHT/6, " ");
 		player1Ready = false;
-		savePlayer2 = new TextButton(3 * Constants.WINDOW_WIDTH/4, Constants.WINDOW_HEIGHT/2, "Save Player");
 		player2Ready = false;
+		
 	
 		font = new Font(18, 62, 110, 30, Typeface.SERIF, Typeface.BOLD);
 		font.setTextAlign(Align.CENTER);
 		
-		sheepPlayer1 = new Player(new PlayerGfx(Constants.sheep1), player1Name, 0);
+		sheepPlayer1 = new Player(new PlayerGfx(Constants.sheep1), playerName, 0);
 		sheepPlayer1.getGfx().setPosition(Constants.WINDOW_WIDTH/8, 4*Constants.WINDOW_HEIGHT/10);
-		sheepPlayer2 = new Player(new PlayerGfx(Constants.sheep1), player2Name, 0);
-		sheepPlayer2.getGfx().setPosition(5*Constants.WINDOW_WIDTH/8, 4*Constants.WINDOW_HEIGHT/10);
-
 		this.main = main;
+		
 //		this.index1 = Game.getGameObject().getPlayers().indexOf(players[0]);
 //		this.index2 = Game.getGameObject().getPlayers().indexOf(players[1]);
 		
@@ -67,10 +64,7 @@ public class InitGameView extends State implements KeyboardListener{
 	@Override
 	public boolean onKeyDown(KeyEvent e){
 		System.out.println("knapp trykket "+(char)e.getUnicodeChar());
-		if(!player1Ready)
-			player1Name+=Character.toString((char)e.getUnicodeChar());
-		else
-			player2Name+=Character.toString((char)e.getUnicodeChar());
+			playerName+=Character.toString((char)e.getUnicodeChar());
 		return true;
 	}
 	
@@ -80,20 +74,17 @@ public class InitGameView extends State implements KeyboardListener{
 		backButton.draw(canvas);
 		startGame.draw(canvas);
 		sheepPlayer1.getGfx().draw(canvas);
-		sheepPlayer2.getGfx().draw(canvas);
 		
 		Constants.redBox.draw(canvas, Constants.WINDOW_WIDTH/2 - 15, Constants.WINDOW_HEIGHT/6);
 		Constants.whiteBox.draw(canvas, Constants.WINDOW_WIDTH/2 - 45, Constants.WINDOW_HEIGHT/6);
 		Constants.greenBox.draw(canvas, Constants.WINDOW_WIDTH/2 + 15, Constants.WINDOW_HEIGHT/6);
 		Constants.blueBox.draw(canvas, Constants.WINDOW_WIDTH/2 + 45, Constants.WINDOW_HEIGHT/6);
 
-		savePlayer1.draw(canvas);
-		savePlayer2.draw(canvas);
+		savePlayer.draw(canvas);
+		player1ReadyButton.draw(canvas);
 
-		canvas.drawText("Player Name:", 3*Constants.WINDOW_WIDTH/4, Constants.WINDOW_HEIGHT/6, font);
 		canvas.drawText("Player Name:", Constants.WINDOW_WIDTH/4, Constants.WINDOW_HEIGHT/6, font);
-		canvas.drawText(player2Name, 3*Constants.WINDOW_WIDTH/4, 2*Constants.WINDOW_HEIGHT/6, font);
-		canvas.drawText(player1Name, Constants.WINDOW_WIDTH/4, 2*Constants.WINDOW_HEIGHT/6, font);
+		canvas.drawText(playerName, Constants.WINDOW_WIDTH/4, 2*Constants.WINDOW_HEIGHT/6, font);
 	}
 	
 	@SuppressLint("NewApi")
@@ -106,72 +97,51 @@ public class InitGameView extends State implements KeyboardListener{
 			getGame().popState();
 			}
 		else if(startGame.onTouchDown(event) && player1Ready && player2Ready ){
-			if(player1Ready && player2Ready){
-				if(!player1Name.isEmpty() || !player2Name.isEmpty()){
-					MyGame.getGameObject().getPlayers().add(sheepPlayer1);
-					MyGame.getGameObject().getPlayers().add(sheepPlayer2);
-					}
-				else{
-					player1Name = "P1";
-					player2Name = "P2";
-					MyGame.getGameObject().getPlayers().add(sheepPlayer1);
-					MyGame.getGameObject().getPlayers().add(sheepPlayer2);
-					}
-				
 				// Hide the keyboard when going to another state
 				imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT,0);
 				getGame().pushState(new GameBoardView(main,index1,index2, l));
 				}
-			}
 		else if(event.getX()>Constants.WINDOW_WIDTH/2-15 && event.getX()<Constants.WINDOW_WIDTH/2+Constants.redBox.getWidth()-15 
 				&& event.getY()>Constants.WINDOW_HEIGHT/6 && event.getY()<Constants.WINDOW_HEIGHT/5+Constants.redBox.getHeight()){
-				if(player1Ready){
-					sheepPlayer2.getGfx().setView(Constants.redSheep);
-					}
-				else{
-					sheepPlayer1.getGfx().setView(Constants.redSheep);
-					}
+				sheepPlayer1.getGfx().setView(Constants.redSheep);
 			}
 		else if(event.getX()>Constants.WINDOW_WIDTH/2-45 && event.getX()<Constants.WINDOW_WIDTH/2+Constants.whiteBox.getWidth()-45
 				&& event.getY()>Constants.WINDOW_HEIGHT/6 && event.getY()<Constants.WINDOW_HEIGHT/6+Constants.whiteBox.getHeight()){
-				if(player1Ready){
-					sheepPlayer2.getGfx().setView(Constants.sheep1);
-					}
-				else{
-					sheepPlayer1.getGfx().setView(Constants.sheep1);
-					}
+				sheepPlayer1.getGfx().setView(Constants.sheep1);
 			}
 		else if(event.getX()>Constants.WINDOW_WIDTH/2+15 && event.getX()<Constants.WINDOW_WIDTH/2+Constants.greenBox.getWidth()+15
 				&& event.getY()>Constants.WINDOW_HEIGHT/6 && event.getY()<Constants.WINDOW_HEIGHT/6+Constants.greenBox.getHeight()){
-				if(player1Ready){
-					sheepPlayer2.getGfx().setView(Constants.greenSheep);
-					}
-				else{
-					sheepPlayer1.getGfx().setView(Constants.greenSheep);
-					}
+				sheepPlayer1.getGfx().setView(Constants.greenSheep);
 			}
 		else if(event.getX()>Constants.WINDOW_WIDTH/2+45 && event.getX()<Constants.WINDOW_WIDTH/2+Constants.blueBox.getWidth()+45
 				&& event.getY()>Constants.WINDOW_HEIGHT/6 && event.getY()<Constants.WINDOW_HEIGHT/6+Constants.blueBox.getHeight()){
-				if(player1Ready){
-					sheepPlayer2.getGfx().setView(Constants.blueSheep);
-					}
-				else{
-					sheepPlayer1.getGfx().setView(Constants.blueSheep);
-					}
+				sheepPlayer1.getGfx().setView(Constants.blueSheep);
 			}
-		if(savePlayer1.onTouchDown(event)){
-			player1Ready = true;
-			savePlayer1.setLabel("Player ready!");
-		}
-		if(savePlayer2.onTouchDown(event) && player1Ready){
-			player2Ready = true;
-			savePlayer2.setLabel("Player ready!");
+		if(savePlayer.onTouchDown(event)){
+			if(player1Ready){
+				if(playerName.isEmpty()){
+					playerName = "P2";
+				}
+				player2Ready = true;
+				savePlayer.setLabel("Player 2 ready!");
+				MyGame.getGameObject().getPlayers().add(sheepPlayer1);
+			}
+			else{
+				if(playerName.isEmpty()){
+					playerName = "P1";
+				}
+				player1Ready = true;
+				player1ReadyButton.setLabel("Player 1 ready!");
+				MyGame.getGameObject().getPlayers().add(sheepPlayer1);
+				playerName = "";
+				sheepPlayer1 = new Player(new PlayerGfx(Constants.sheep1), playerName, 0);
+				sheepPlayer1.getGfx().setPosition(Constants.WINDOW_WIDTH/8, 4*Constants.WINDOW_HEIGHT/10);
+			}
 		}
 		return true;
 	}
 	
 	public void update(float dt){
 		sheepPlayer1.getGfx().update(dt);
-		sheepPlayer2.getGfx().update(dt);
 	}
 }
