51e380b079e76c6d9d0c93327ff936bd429bba20
trulsbjo <truls.hamborg@gmail.com>
2013-04-25 10:55:17 +0200

    fixed error with touch in QuestionView

--- src/sheeprace/derp/QuestionView.java
+++ src/sheeprace/derp/QuestionView.java
@@ -15,96 +15,113 @@ import sheep.gui.TextButton;
 
 /**
  * A view that displays the question and what answers are available.
- *
+ * 
  */
 
-public class QuestionView extends State{
+public class QuestionView extends State {
 	private String question;
 	private MainActivity main;
 	private Font font;
 	private Question q;
 	private List<TextButton> possibleAnswers;
 	private boolean correct;
-	
-	public QuestionView(MainActivity main){
+
+	public QuestionView(MainActivity main) {
 		q = MyGame.getGameObject().getNextQuestion();
-		
+
 		font = new Font(18, 62, 110, 30, Typeface.SERIF, Typeface.BOLD);
 		font.setTextAlign(Align.CENTER);
 		this.main = main;
 		int fontInt = 150;
 		possibleAnswers = new ArrayList<TextButton>();
 		for (int i = 0; i < q.getOptions().size(); i++) {
-			TextButton t = new TextButton(Constants.WINDOW_WIDTH/2, fontInt, q.getOptions().get(i));
+			TextButton t = new TextButton(Constants.WINDOW_WIDTH / 2, fontInt,
+					q.getOptions().get(i));
 			possibleAnswers.add(t);
 			fontInt += 50;
 		}
 		this.correct = false;
 
 	}
-	
-	public void draw(Canvas canvas){
+
+	public void draw(Canvas canvas) {
 
 		canvas.drawColor(Color.RED);
 		canvas.drawBitmap(Constants.background_new, 0, 0, null);
-		canvas.drawText("" + q.getQuestion() , Constants.WINDOW_WIDTH/2, 100, font);
+		canvas.drawText("" + q.getQuestion(), Constants.WINDOW_WIDTH / 2, 100,
+				font);
 		for (int i = 0; i < possibleAnswers.size(); i++) {
 			possibleAnswers.get(i).draw(canvas);
 		}
 
-	//	System.out.println("During question" +MyGame.getGameObject().getPlayer().getGfx().getPosition() );
+		// System.out.println("During question"
+		// +MyGame.getGameObject().getPlayer().getGfx().getPosition() );
 	}
-	
-	public String getQuestion(){
+
+	public String getQuestion() {
 		return this.question;
 	}
-	
-	public void viewQuestion(){
+
+	public void viewQuestion() {
 		System.out.println(question);
 	}
-	
-	public boolean onTouchDown(MotionEvent evt){		
+
+	public boolean onTouchDown(MotionEvent evt) {
 		for (int i = 0; i < possibleAnswers.size(); i++) {
-			if(possibleAnswers.get(i).onTouchDown(evt) && q.getCorrectA().get(i)){
-				if(MyGame.getGameObject().isPlayer1sTurn())
-					MyGame.getGameObject().p1IsCorrect();
-				else
-					MyGame.getGameObject().p2sCorrect();
+			if (possibleAnswers.get(i).onTouchDown(evt)) {
+
+				correct = q.getCorrectA().get(i);
+
+				if (correct) {
+
+					if (MyGame.getGameObject().isPlayer1sTurn())
+						MyGame.getGameObject().p1IsCorrect();
+					else
+						MyGame.getGameObject().p2sCorrect();
+
+				}
+
 				MyGame.getGameObject().getPlayer().increasePowerbarPower(200);
-				correct = true;
+				getGame().pushState(new Splash(main, correct));
+
+				System.out.println("Player 1 score: "
+						+ MyGame.getGameObject().getp1sCorrect());
+				System.out.println("Player 2 score: "
+						+ MyGame.getGameObject().getp2sCorrect());
 			}
 
 		}
 
-		System.out.println("Player 1 score: "+MyGame.getGameObject().getp1sCorrect());
-		System.out.println("Player 2 score: "+MyGame.getGameObject().getp2sCorrect());
-		getGame().pushState(new Splash(main,correct));
 		return true;
 	}
-	
-	//Making a seperate class for the splash upon answering a question
-	class Splash extends State{
+
+	// Making a seperate class for the splash upon answering a question
+	class Splash extends State {
 		MainActivity main2;
 		boolean correct;
-		public Splash(MainActivity main, boolean correct){
+
+		public Splash(MainActivity main, boolean correct) {
 			this.main2 = main;
-			this.correct = correct; 
+			this.correct = correct;
 		}
-		
-		public void draw(Canvas canvas){
-			if(correct)
+
+		public void draw(Canvas canvas) {
+			if (correct)
 				canvas.drawColor(Color.GREEN);
 			else
 				canvas.drawColor(Color.RED);
-			
+
 			long time = System.nanoTime();
 			long longtime = System.nanoTime();
-			while(longtime-time<1500000000){
-//				System.out.println(longtime-time);
+			while (longtime - time < 1500000000) {
+				// System.out.println(longtime-time);
 				longtime = System.nanoTime();
 			}
 			getGame().popState(2);
-			System.out.println("After question: "+MyGame.getGameObject().getPlayer().getGfx().getPosition());
+			System.out
+					.println("After question: "
+							+ MyGame.getGameObject().getPlayer().getGfx()
+									.getPosition());
 		}
 	}
 }
